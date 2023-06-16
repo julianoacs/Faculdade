ed5ada97d6fddaa495fdae3cd3d0912b2a34aaef
Juliano Silva <julianoacsilva@hotmail.com>
2023-06-15 22:30:04 -0300

    Adicionado Exercicio3

---
+++ Exercicio3/src/exercicio3/Exercicio3.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercicio3;
+
+//@author juliano
+
+import java.util.Scanner;
+
+
+public class Exercicio3 {
+
+ 
+    public static void main(String[] args) {
+       Scanner input = new Scanner (System.in);
+        int numero1 = 0;
+        int numero2 = 0;
+        int numero3 = 0;
+        int resultado = 0;
+        int resultado2 = 0;
+        int resultado3 = 0;
+        
+        
+    System.out.println("Digite um numero ");
+    numero1 = input.nextInt();
+    System.out.println("Digite outro numero ");
+    numero2 = input.nextInt();
+    System.out.println("Digite outro numero ");
+    numero3 = input.nextInt();
+        
+    resultado = numero1 + numero2 + numero3;
+    resultado2 = resultado /3;
+    resultado3 = numero1 * numero2 * numero3;
+    
+    System.out.println("A soma dos valores e " + resultado);
+    System.out.println("O Produto dos valores e " + resultado2);
+    System.out.println("O Produto dos valores é " + resultado3);
+
+    }
+    
+}
