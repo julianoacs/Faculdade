f78ebe2193004695acad9626af3866575cc3323c
Juliano Silva <123031321+julianoacs@users.noreply.github.com>
2023-07-29 23:56:33 -0300

    Alteracao

---
+++ Exercicios/exercicio1/Exercicio1.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercicio1;
+
+// @author Julianoacs
+
+/*Escreva um aplicativo que solicite ao usuário inserir dois inteiros,
+obtenha dele esses números e imprima sua soma, produto, diferença e quociente (divisão).*/
+
+import java.util.Scanner;
+
+
+public class Exercicio1 {
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        int numero1 = 0;
+        int numero2 = 0;
+        int resultado = 0;
+        int resultado2 = 0;
+        int resultado3 = 0;
+        int resultado4 = 0;
+        
+        
+    System.out.println("Digite um numero ");
+    numero1 = input.nextInt();
+    System.out.println("Digite outro numero ");
+    numero2 = input.nextInt();
+        
+    resultado = numero1 + numero2;
+    resultado2 = numero1 * numero2;
+    resultado3 = numero1 - numero2;
+    resultado4 = numero1 / numero2;
+    
+    System.out.println("A soma dos valores e " + resultado);
+    System.out.println("O Produto dos valores e " + resultado2);
+    System.out.println("A diferenca entre os valores e " + resultado3);
+    System.out.println("O quociente entre os valores e " + resultado4);
+    }
+ }
+   
---
+++ Exercicios/exercicio3/Exercicio3.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercicio3;
+
+//@author Julianoacs
+
+/*Escreva um aplicativo que insira três inteiros digitados pelo usuário e
+exiba a soma, média, produto e os números menores e maiores.*/
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+    public static void main(String[] args) {
+       Scanner input = new Scanner (System.in);
+        int numero1 = 0;
+        int numero2 = 0;
+        int numero3 = 0;
+        int resultado = 0;
+        int resultado2 = 0;
+        int resultado3 = 0;
+        
+        
+    System.out.println("Digite um numero ");
+    numero1 = input.nextInt();
+    System.out.println("Digite outro numero ");
+    numero2 = input.nextInt();
+    System.out.println("Digite outro numero ");
+    numero3 = input.nextInt();
+        
+    resultado = numero1 + numero2 + numero3;
+    resultado2 = resultado /3;
+    resultado3 = numero1 * numero2 * numero3;
+    
+    System.out.println("A soma dos valores e " + resultado);
+    System.out.println("O produto dos valores e " + resultado2);
+    System.out.println("O multiplicacao dos valores e " + resultado3);
+    }
+}
