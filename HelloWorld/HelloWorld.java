2d4f7c495d8d26afa5e1da91fd65db90714714fa
Juliano Silva <123031321+julianoacs@users.noreply.github.com>
2023-07-31 21:46:39 -0300

    Create HelloWorld.java

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package helloworld;
+
+//@author Julianoacs
+
+//Escrevendo Hello, World! na tela, primeiro programa.
+ 
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        //sout+tab = System.out.println
+        
+    }
+    
+}
