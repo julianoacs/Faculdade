a93657706b91a4180583329039cc32ad0db335de
Juliano Silva <123031321+julianoacs@users.noreply.github.com>
2024-02-28 13:44:18 -0300

    OO

---
+++ oo-exercicio-01/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    //Crie uma classe Pessoa com um método que exibe "Olá, mundo!" no console.
+    public static void main(String[] args) {
+
+            Pessoa pessoa = new Pessoa();
+            pessoa.saudacao();
+    }
+}
