1436dc618f653e06647262c07bfbd82a095bbb53
Juliano Silva <123031321+julianoacs@users.noreply.github.com>
2024-03-14 10:50:31 -0300

    Alura

---
+++ Alura/aula004-projetofinal/src/Principal.java
@@ -0,0 +1,10 @@
+import br.com.alura.projetofinal.modelos.Musica;
+
+public class Principal {
+    public static void main(String[] args) {
+        Musica musica1 = new Musica();
+        musica1.setTitulo("It Was A Good Day");
+        musica1.setArtista("Ice Cube");
+
+    }
+}
