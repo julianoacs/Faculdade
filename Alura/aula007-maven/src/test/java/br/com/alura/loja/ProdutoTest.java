0f8c7b43cc09c8c0fbfe1c18276592df87499bf8
Juliano Silva <123031321+julianoacs@users.noreply.github.com>
2024-03-25 13:33:37 -0300

    Alura
    Adicionado aula de maven

---
+++ Alura/aula007-maven/src/test/java/br/com/alura/loja/ProdutoTest.java
@@ -0,0 +1,15 @@
+package br.com.alura.loja;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.w3c.dom.ls.LSOutput;
+
+public class ProdutoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	//ls
+}
