49b004a315dc839fb9e644b879c43895465773f6
Juliano Silva <123031321+julianoacs@users.noreply.github.com>
2024-04-10 20:01:09 -0300

    Alura
    Alura

--- Alura/exercicio03/src/Exercicio3.java
+++ Alura/exercicio03/src/Exercicio3.java
@@ -1,6 +1,6 @@
 public class Exercicio3 {
     public static void main(String[] args){
-        String palavra = "Juliano";
+        String palavra = "Juliano Silva";
         char letra = 'S';
         System.out.println(palavra + " " + letra);
     }
